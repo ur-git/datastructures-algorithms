64387741276cf0c8054afca2da3cc73bc0b466db
urvashi <rawaturvashi68@gmail.com>
2025-02-17 23:04:36 +0530

    add problem_solve js examples

--- Problem_solving_pattern/Code/sum_zero.js
+++ Problem_solving_pattern/Code/sum_zero.js
@@ -1,1 +1,28 @@
-// Write a function `sumZero` that accepts a **sorted array** of integers. The function should find the **first pair** where the sum is zero and return it as an array. If no such pair exists, return `undefined`.
+// Write a function `sumZero` that accepts a sorted array of integers. The function should find the first pair where the sum is zero and return it as an array. If no such pair exists, return `undefined`.
+// | Left Pointer | Right Pointer | Sum | Action                 |
+// | ------------ | ------------- | --- | ---------------------- |
+// | -4 (0)       | 5 (7)         | 1   | Decrease `right`       |
+// | -4 (0)       | 2 (6)         | -2  | Increase `left`        |
+// | -3 (1)       | 2 (6)         | -1  | Increase `left`        |
+// | -2 (2)       | 2 (6)         | 0   | Found! Return `[-2,2]` |
+
+function sumZero(arr, addValue) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let sum = arr[right] + arr[left];
+
+    if (sum === addValue) {
+      return [arr[right], arr[left]];
+    } else if (sum > addValue) {
+      right--;
+    } else if (sum < addValue) {
+      left++;
+    }
+  }
+
+  return undefined;
+}
+
+console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 5], 1));
